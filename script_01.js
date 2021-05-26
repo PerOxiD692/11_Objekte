1485df95c99d352da06f910c73099fce516c5487
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-05-26 14:41:16 +0200

    objekte2 ++

--- script_01.js
+++ script_01.js
@@ -28,11 +28,28 @@ let person = {
 // ausgabe(person.sayHello());
 
 let txt = "ich bin " + person.firstName + " " + person.familyName + " und verdiene " + person.salary[1] + " p.a";
-ausgabe(txt);
+// ausgabe(txt);
+
+
+/***** Objekte 2 *****/
+
+// Tiefer, tiefer - Irgendwo in der Tiefe gibt es ein Licht // K.Bush
+// Der Baikalsee ist mit 1642 m der tiefste See der Erde. ...
+// deep - deeper - deepest 
+// Nested Object 
+
+let baikal = {
+
+                wert: "10m",
+                deep: { deeper: { deepest: "Das Licht - auf 1642m"}
+                      }
+};
+
+ausgabe(baikal);
+ausgabe(baikal.wert);
+ausgabe(baikal.deep.deeper.deepest);
 
 /** Ausgabe */
 function ausgabe(outputStr) {
-        console.log(outputStr);
-    }
-
-
+    console.log(outputStr);
+}
